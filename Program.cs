9aeee436eed6bfe1821c59f6a4017f5f29e1ad7c
yayo <yayosoup@gmail.com>
2026-02-03 21:52:45 -0800

    m

--- Program.cs
+++ Program.cs
@@ -1,31 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-app.MapGet("/health", () => Results.Ok("ok"));
 
-
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// IMPORTANT: do this before HTTPS redirection
+app.MapGet("/health", () => Results.Ok("ok"));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapDefaultControllerRoute();
 app.Run();
+
